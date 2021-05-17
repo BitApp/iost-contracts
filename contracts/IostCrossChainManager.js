c2cf91efba425727a92d1dab91315aaf9a55e82e
hffp <510033939@qq.com>
2021-05-17 18:14:57 +0800

    just fuck

--- contracts/IostCrossChainManager.js
+++ contracts/IostCrossChainManager.js
@@ -12,9 +12,20 @@ class IostCrossChainManager {
   }
 
   // can be call for everyone ?
+  // rawHeader: byte[]
+  // pubkeyList: byte[]
+  // can be use hex str, Buffer.form()
   initGenesisBlock(rawHeader, pubKeyList) {
     this._checkKey(INIT_STATUS_KEY, false, "has been init");
+    const header = this._deserializerHeader(rawHeader);
+    const bookKeeper = this._verifyPubKey(pubKeyList);
+    if (header.nextBookKeeper !== bookKeeper.nextBookKeeper) {
+      throw "nextBookers illegal"
+    }
+    this._setCurrentEpochHeight(header.height);
+    this._put(INIT_STATUS_KEY, 1);
 
+    blockchain.event(JSON.stringify({height: header.height, rawHeader}))
   }
 
   _contractOwnerAuth() {
@@ -30,7 +41,10 @@ class IostCrossChainManager {
     }
   }
 
-  _deserializerHeader (data) {
+  // source: byte[]
+  _deserializerHeader (source) {
+    let offset = 0;
+    const version = new Int64(source.slice(offset, 4));
     return {}
   }
 
@@ -45,10 +59,40 @@ class IostCrossChainManager {
     return this._getBookKeeper(n, n - (n -1)/3, pubkeyList)
   }
 
+  _setCurrentEpochHeight(height) {
+    this._put("currentEpochHeight", height)
+  }
+
   _getBookKeeper(keyLen, minSigNum, pubkeyList) {
     let buff = new Uint16Array(keyLen);
     const keepers = new Array(20);
+    for (let i = 0; i < keyLen; i++) {
+      buff = pubkeyList.slice(i * POLYCHAIN_PUBKEY_LEN, POLYCHAIN_PUBKEY_LEN);
+      keepers[i] = pubkeyList.slice(i * POLYCHAIN_PUBKEY_LEN, POLYCHAIN_PUBKEY_LEN).slice(3, 64);
+    }
+  }
 
+  _writeVarBytes(source, target) {
+    return _writeVarInt(target.length, source).concat(target)
+  }
+
+  _writeVarInt(value, source) {
+    if (value < 0) {
+      return  source;
+    }else if (value < 0xFD) {
+      var v = value.to
+    } else if (value <=0xFFFF) {
+
+    } else if (value <= 0XFFFFFFFF) {
+
+    } else {
+
+    }
+  }
+
+  // key,value string
+  _put(key, value){
+    storage.put(key, value, tx.publisher)
   }
 }
 
